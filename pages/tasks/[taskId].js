944d7e17f0dcd299d13f35f002f02ad30a1535a7
gourab <rgourab02@gmail.com>
2022-12-29 16:07:14 +0600

    may be fixed

--- pages/tasks/[taskId].js
+++ pages/tasks/[taskId].js
@@ -5,8 +5,15 @@ import { useEffect, useState } from "react";
 import IndividualTask from "../../components/IndividualTask";
 
 
-const TaskId = ({ task }) => {
-   
+const TaskId = () => {
+  const router=useRouter()
+const id=router.query.taskId
+const [task,setTask]=useState([])
+useEffect(()=>{
+  fetch(`https://my-todo-server-nu.vercel.app/tasks/${id}`)
+  .then(res=>res.json())
+  .then(data=>setTask(data))
+},[task])
 
   return (
    
@@ -21,34 +28,47 @@ const TaskId = ({ task }) => {
   );
 };
 
-export const getStaticProps = async (context) => {
-  const { params } = context;
-  const res = await fetch(
-    `https://my-todo-server-nu.vercel.app/tasks/${params?.taskId}`
-  );
-  const data = await res.json();
+// export const getServerSideProps= async ()=>{
+  
 
-  return {
-    props: {
-      task: data,
-    },
-  };
-};
+//   const res= await fetch(`https://my-todo-server-nu.vercel.app/tasks/${id}`)
+//   const data= await res.json()
+//   console.log(data);
+//   return{
+//      props:{
+//          task: data
+//      }
+//   }
+//  }
 
-export const getStaticPaths = async () => {
-  const res = await fetch("https://my-todo-server-nu.vercel.app/tasks");
-  const tasks = await res.json();
-  const paths = tasks?.map((task) => {
-    return {
-      params: {
-        taskId: `${task._id}`,
-      },
-    };
-  });
-  return {
-    paths,
-    fallback: false,
-  };
-};
+// export const getStaticProps = async (context) => {
+//   const { params } = context;
+//   const res = await fetch(
+//     `https://my-todo-server-nu.vercel.app/tasks/${params?.taskId}`
+//   );
+//   const data = await res.json();
+
+//   return {
+//     props: {
+//       task: data,
+//     },
+//   };
+// };
+
+// export const getStaticPaths = async () => {
+//   const res = await fetch("https://my-todo-server-nu.vercel.app/tasks");
+//   const tasks = await res.json();
+//   const paths = tasks?.map((task) => {
+//     return {
+//       params: {
+//         taskId: `${task._id}`,
+//       },
+//     };
+//   });
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// };
 
 export default TaskId;
